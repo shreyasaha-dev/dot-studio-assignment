1159edb05f8fef2611b0adf3953369e869fd6ad9
shreyasaha-dev <shreyasaha562@gmail.com>
2024-04-30 21:31:34 +0530

    done

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const cardData = [
     backgroundImage: "./assets/images/banner1.png",
     title:
       '<p class="title-hide">Holistic <span class="yellow-text">Development</span></p>',
-    heading: '<h2>Holistic <span class="yellow-text">Development</span></h2>',
+    heading:
+      '<h2 class="show-heading">Holistic <span class="yellow-text">Development</span></h2>',
     subHeading: "Right Mix of Curricular and Co-Curricular Activities.",
   },
   {
@@ -11,7 +12,7 @@ const cardData = [
     title:
       '<p class="title-hide"><span class="yellow-text">Proven</span> and <span class="yellow-text">Tested</span> Centralized Curriculum</p>',
     heading:
-      '<h2><span class="yellow-text">Proven</span> and <span class="yellow-text">Tested</span> Centralized Curriculum</h2>',
+      '<h2 class="show-heading"><span class="yellow-text">Proven</span> and <span class="yellow-text">Tested</span> Centralized Curriculum</h2>',
     subHeading:
       "CBSE school with balanced traditional & contemporary curriculum to foster growth in all its students.",
   },
@@ -20,7 +21,7 @@ const cardData = [
     title:
       '<p class="title-hide"><span class="yellow-text">Innovative</span> and <span class="yellow-text">Challenging</span> learning Methodologies</p>',
     heading:
-      '<h2><span class="yellow-text">Innovative</span> and <span class="yellow-text">Challenging</span> learning Methodologies</h2>',
+      '<h2 class="show-heading"><span class="yellow-text">Innovative</span> and <span class="yellow-text">Challenging</span> learning Methodologies</h2>',
     subHeading:
       "Facilitate excellence through critical thinking and profound learning.",
   },
@@ -29,7 +30,7 @@ const cardData = [
     title:
       '<p class="title-hide"><span class="yellow-text">Faculty</span> Enrichment Program</p>',
     heading:
-      '<h2><span class="yellow-text">Faculty</span> Enrichment Program</h2>',
+      '<h2 class="show-heading"><span class="yellow-text">Faculty</span> Enrichment Program</h2>',
     subHeading: "Regular teacher training and curriculum workshops",
   },
 ];
@@ -54,7 +55,7 @@ cardData.forEach((card) => {
   ${card.title}
   <div class="hover-details">
     ${card.heading}
-    <p>${card.subHeading}</p>
+    <p class="show-sub-heading">${card.subHeading}</p>
   </div>
 </div>
     `;
